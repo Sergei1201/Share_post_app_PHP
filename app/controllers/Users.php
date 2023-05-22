7c1385abdc4f55bb6ad2cbf8575c427be78cd37f
Sergei Sokolov <sokolov12ssa@gmail.com>
2023-05-22 23:59:24 +0500

    Input validation

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,5 +1,5 @@
 <?php
-// Create a users class to register, login, etc
+// Create a users controller to register, login, etc
 class Users extends Controller
 {
     // Constructor
@@ -7,15 +7,52 @@ class Users extends Controller
     {
     }
 
-    // Register a user
+    // Register user
     public function register()
     {
-
         // Check if the method is POST
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Process form
+            // Sanitize data
+            $_POST = filter_input_array(INPUT_POST);
+            // Initialize data
+            $data = [
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirm_password' => trim($_POST['confirm_password']),
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => ''
+            ];
+            // Validate data
+            if (empty($data['name'])) {
+                $data['name_err'] = 'Please enter your name';
+            }
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter your email';
+            }
+            if (empty($data['password'])) {
+                $data['password_err'] = 'Please enter your password';
+            } elseif (strlen($data['password']) < 6) {
+                $data['password_err'] = 'Password must be at least 6 characters';
+            }
+            if (empty($data['confirm_password'])) {
+                $data['confirm_password_err'] = 'Please confirm password';
+            } else {
+                if ($data['password'] !== $data['confirm_password']) {
+                    $data['confirm_password_err'] = 'Passwords do not match';
+                }
+            }
+            // Check if there are not any errors
+            if (empty($data['name_err']) && empty($data['email_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])) {
+                die('Success');
+            } else {
+                $this->view('users/register', $data);
+            }
         } else {
-            // Define user data as an associative array
+            // Initialize data variable to represent users input as an associative array
             $data = [
                 'name' => '',
                 'email' => '',
@@ -26,18 +63,40 @@ class Users extends Controller
                 'password_err' => '',
                 'confirm_password_err' => ''
             ];
-            // Load form 
-            $this->view('/users/register', $data);
+            // Load form
+            $this->view('users/register', $data);
         }
     }
+
+
     // Login user
     public function login()
     {
         // Check if the method is POST
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            // Process the form
+            // Process form
+            $_POST = filter_input_array(INPUT_POST);
+            // Initialize data
+            $data = [
+                'email' => trim($_POST['email']),
+                'password' => trim(($_POST['password'])),
+                'email_err' => trim(($_POST['email_err'])),
+                'password_err' => trim(($_POST['password_err']))
+            ];
+            // Validate input
+            if (empty($data['email'])) {
+                $data['email_err'] = 'Please enter email';
+            }
+            if (empty($data['password'])) {
+                $data['password_err'] = 'Please enter password';
+            }
+            if (empty($data['email_err']) && empty($data['password_err'])) {
+                die('Success');
+            } else {
+                $this->view('users/login', $data);
+            }
         } else {
-            // Define user data as an associative array
+            // Initialize data variable as an associative array
             $data = [
                 'email' => '',
                 'password' => '',
@@ -45,7 +104,7 @@ class Users extends Controller
                 'password_err' => ''
             ];
             // Load form
-            $this->view('/users/login', $data);
+            $this->view('users/login', $data);
         }
     }
 }
